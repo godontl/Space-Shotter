9c41b91f53c27046eec448703b9ca1ca50b36c49
Lorenzo Lusenti <l.lusenti@studenti.unibs.it>
2024-10-30 16:38:04 +0100

    Add files via upload

---
+++ src/it/unibs/eps/spaceshooter/SpaceShooterTester.java
@@ -0,0 +1,14 @@
+package it.unibs.eps.spaceshooter;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
+import javax.swing.*;
+public class SpaceShooterTester {
+
+	public static void main(String[] args) {
+		
+	}
+}
